58d2f967b78c0f47ed0322ebf265d5b105affa65
liwy <liwenyao_java@126.com>
2017-08-24 17:43:48 +0800

    初步完成水平滑动的简单功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
-/*! liwy-slide - v1.0.0 - 2017-08-20
+/*! liwy-slide - v1.0.0 - 2017-08-24
 * https://github.com/liwyspace/liwy-slide#readme
 * Copyright (c) 2017 liwy; Licensed MIT */
 $(function(){
-	$("#liwy-slide div").liwySlide({'color':'#008888','fontSize':'12px'});
+	$("#liwy-slide").liwySlide();
 });
